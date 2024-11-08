03332882bbedb87925e461665257a6df60dc2a76
Perelyn <64838956+Perelyn-sama@users.noreply.github.com>
2024-11-08 03:44:48 +0100

    add template url to NewArgs

--- cli/src/args.rs
+++ cli/src/args.rs
@@ -4,6 +4,13 @@ use clap::{arg, Parser};
 pub struct NewArgs {
     #[arg(value_name = "NAME", help = "The name of the program")]
     pub name: Option<String>,
+
+    #[arg(
+        long = "template",
+        value_name = "URL",
+        help = "Git repository URL containing program templates (optional)"
+    )]
+    pub template_url: Option<String>,
 }
 
 #[derive(Parser, Debug)]
