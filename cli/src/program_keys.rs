174d0ae051c4187c8be1842b70cf41b62a03dfee
Jimii <30603522+jim4067@users.noreply.github.com>
2025-01-28 18:45:50 +0300

    Jimii/keys subcmd (#40)
    * feat: working with keypairs
    
    list command to print program public key
    
    * feat: generate new keypair
    
    * feat: sync keypair
    
    * chore: println changes
    
    ---------
    
    Co-authored-by: Hardhat Chad <155858888+HardhatChad@users.noreply.github.com>

---
+++ cli/src/program_keys.rs
@@ -0,0 +1,105 @@
+use solana_sdk::{signature::Keypair, signer::Signer};
+use std::{
+    fs::{self},
+    io::Write,
+};
+
+use crate::{
+    utils::{get_project_name, is_project_built, is_valid_steel_project, replace_prog_id},
+    ProgramKeysArgs,
+};
+
+pub fn list_keypair(_args: ProgramKeysArgs) -> anyhow::Result<()> {
+    if !is_valid_steel_project()? {
+        anyhow::bail!("Not a valid Steel project!");
+    }
+
+    if !is_project_built()? {
+        anyhow::bail!("Please build project first by running `steel build`!");
+    }
+
+    let project_name = get_project_name()?;
+    let formatted = project_name.replace("-", "_");
+    let deploy_kp_path = format!("./target/deploy/{}-keypair.json", formatted);
+
+    // read public key
+    let keypair_file = fs::read_to_string(deploy_kp_path)?;
+
+    let keypair_bytes: Vec<u8> = serde_json::from_str(&keypair_file)?;
+    let keypair = Keypair::from_bytes(&keypair_bytes)?;
+
+    println!("{project_name}: {}", keypair.pubkey().to_string());
+
+    Ok(())
+}
+
+pub fn new_keypair(_args: ProgramKeysArgs) -> anyhow::Result<()> {
+    if !is_valid_steel_project()? {
+        anyhow::bail!("Not a valid Steel project!");
+    }
+
+    if !is_project_built()? {
+        anyhow::bail!("Please build project first by running `steel build`!");
+    }
+
+    // replace declare_id! address
+    let new_key = Keypair::new();
+    replace_prog_id(new_key.insecure_clone().into())?;
+
+    // replace keypair in deploy
+    let project_name = get_project_name()?;
+    let formatted = project_name.replace("-", "_");
+    let deploy_kp_path = format!("./target/deploy/{}-keypair.json", formatted);
+
+    let mut lib_rs = fs::OpenOptions::new()
+        .create(true)
+        .write(true)
+        .truncate(true)
+        .open(deploy_kp_path)?;
+    lib_rs.write_all(&format!("{:?}", new_key.to_bytes()).as_bytes())?;
+    lib_rs.flush()?;
+
+    println!("{project_name}: {}", new_key.pubkey().to_string());
+
+    Ok(())
+}
+
+pub fn sync_keypair(_args: ProgramKeysArgs) -> anyhow::Result<()> {
+    if !is_valid_steel_project()? {
+        anyhow::bail!("Not a valid Steel project!");
+    }
+
+    if !is_project_built()? {
+        anyhow::bail!("Please build project first by running `steel build`!");
+    }
+
+    // read keypair file
+    let project_name = get_project_name()?;
+    let formatted = project_name.replace("-", "_");
+    let deploy_kp_path = format!("./target/deploy/{}-keypair.json", formatted);
+    let deploy_kp_contents = fs::read_to_string(deploy_kp_path.clone())?;
+    let keypair_bytes: Vec<u8> = serde_json::from_str(&deploy_kp_contents)?;
+    let public_key = Keypair::from_bytes(&keypair_bytes)?.pubkey();
+
+    // check if it matches lib.rs
+    let lib_rs_contents = fs::read_to_string("./api/src/lib.rs")?;
+    let found = lib_rs_contents.find(public_key.to_string().as_str());
+    if found.is_some() {
+        println!("program keys already synced: {}", public_key.to_string());
+        return Ok(());
+    }
+
+    // update with this keypair in lib.rs
+    let new_key = Keypair::new();
+    replace_prog_id(new_key.insecure_clone().into())?;
+    let mut lib_rs = fs::OpenOptions::new()
+        .create(true)
+        .write(true)
+        .truncate(true)
+        .open(deploy_kp_path)?;
+    lib_rs.write_all(&format!("{:?}", new_key.to_bytes()).as_bytes())?;
+    lib_rs.flush()?;
+    println!("program keys synced to: {}", new_key.pubkey().to_string());
+
+    Ok(())
+}
--- cli/src/utils.rs
+++ cli/src/utils.rs
@@ -1,3 +1,10 @@
+use solana_sdk::{signature::Keypair, signer::Signer};
+use std::{
+    fs::{self},
+    io::Write,
+};
+use toml::Value;
+
 pub fn prompt(prompt: &str) -> String {
     println!("{}", prompt);
 
@@ -46,4 +53,120 @@ pub fn capitalize_first(input: &str) -> String {
         None => String::new(),
         Some(first) => first.to_uppercase().collect::<String>() + c.as_str(),
     }
+}
+
+/// Determines if valid steel project.
+///
+/// checks:
+/// 1. The folder structure contains `api` & `program` dirs.
+/// 2. The presence of the `steel` dependency in `Cargo.toml`.
+///
+/// # Returns
+/// - `Ok(true)` if the project is a valid Steel project.
+/// - `Ok(false)` if the project is not a valid Steel project.
+/// - `Err` if `Cargo.toml` cannot be read, parsed, or if the folder structure is invalid.
+///
+pub fn is_valid_steel_project() -> anyhow::Result<bool> {
+    let root_toml = fs::read_to_string("./Cargo.toml")?;
+    let parsed: Value = root_toml.parse().expect("error reading root toml file");
+    let has_steel_dep = parsed
+        .get("workspace")
+        .and_then(|w| w.get("dependencies"))
+        .and_then(|d| d.get("steel"))
+        .is_some();
+
+    let has_api_and_program =
+        has_directories("./", vec!["api".to_string(), "program".to_string()])?;
+
+    Ok(has_steel_dep && has_api_and_program)
+}
+
+/// Retrieves the project name from the `Cargo.toml` file.
+///
+/// # Returns
+/// - `Ok(String)` containing the project name if found.
+/// - `Err` if `Cargo.toml` cannot be read, parsed, or `name` field is missing.
+pub fn get_project_name() -> anyhow::Result<String> {
+    let project_toml = fs::read_to_string("./program/Cargo.toml")?;
+    let parsed: Value = project_toml.parse().expect("Could not parse TOML");
+
+    let name = parsed
+        .get("package")
+        .and_then(|package| package.get("name"))
+        .and_then(|name| name.as_str())
+        .ok_or_else(|| anyhow::anyhow!("Couldn't get package name"))?;
+
+    let name = name.to_string();
+
+    Ok(name)
+}
+
+/// Check if project has been built
+///
+/// # Returns
+/// - `Ok(bool)` true if `target` is found.
+/// - `Err` if `target` dir isn't found
+pub fn is_project_built() -> anyhow::Result<bool> {
+    let has_deploy_dir = has_directories("./target", vec!["deploy".to_string()])?;
+
+    Ok(has_deploy_dir)
+}
+
+/// Checks if a directory contains subdirectories matching specific names.
+///
+/// # Arguments
+/// - `dir`: The path to the directory to scan.
+/// - `dirs`: A list of strings to match against subdirectory names.
+///
+/// # Returns
+/// - `Ok(true)` if the number of matching subdirectories equals `dirs.len()`.
+/// - `Ok(false)` otherwise.
+/// - `Err` if the directory cannot be read o .
+///
+/// # FIXME
+/// - The `contains` method matches partial words (e.g., "ap" or "prog").
+pub fn has_directories(dir: &str, dirs: Vec<String>) -> anyhow::Result<bool> {
+    let expected_count = dirs.len();
+
+    let count = fs::read_dir(dir)?
+        .filter_map(|d| d.ok())
+        .filter(|d| d.path().is_dir())
+        .filter(|d| {
+            let path = d.path();
+            let path_str = path.to_str().unwrap_or("");
+            dirs.iter().any(|dir| path_str.contains(dir)) // fixme: `contains` also matches partial words (e.g., "ap" or "prog").
+        })
+        .count()
+        == expected_count;
+
+    Ok(count)
+}
+
+/// Replaces the declared program id.
+///
+/// # Arguments
+/// - `new_key`: optional `Keypair`. If `None`, new `Keypair` is generated.
+///
+/// # Returns
+/// - `Ok(())` if program ID is successfully replaced.
+/// - `Err` if file cannot be read, written, or if the `declare_id!` macro is not found.
+pub fn replace_prog_id(new_key: Option<Keypair>) -> anyhow::Result<()> {
+    let new_key = new_key.unwrap_or_else(Keypair::new);
+    let lib_rs_path = "./api/src/lib.rs";
+
+    let mut contents = fs::read_to_string(lib_rs_path)?;
+    let offset = contents.find("declare_id!").unwrap_or(contents.len());
+    let offset_start = offset + 11;
+    let formatted_key = format!("(\"{}\");", new_key.pubkey().to_string().as_str());
+    contents.replace_range(offset_start.., &formatted_key);
+
+    // write to file
+    let mut lib_rs = fs::OpenOptions::new()
+        .truncate(true)
+        .write(true)
+        .open(lib_rs_path)?;
+    lib_rs.write_all(contents.as_bytes())?;
+    lib_rs.flush()?;
+
+    Ok(())
 }
